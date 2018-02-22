9f66a8d61044523de7df55bf25fec6fa1c55d6ae
Unknown <zidsod.6840@gmail.com>
2018-02-22 12:34:00 +0700

    1

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+int main(void)
+{
+int  ();
+    printf("Welcome to CLI Calculator V1.00");
 
-int main() {
+
+    int a,b;
+    cout << "menu" << endl;
+    cout << " 1.+ " << endl;
+    cout << " 2.- " << endl;
+    cout << " 3.* " << endl;
+    cout << " 4./ " << endl;
+    cout << " quit " << endl;
+
 
         return 0;
 }
