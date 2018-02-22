5ce17f0a2fdbf1d6fba02968095f1ee3fb26a9f7
Unknown <zidsod.6840@gmail.com>
2018-02-22 12:34:22 +0700

    2

--- exam2.cpp
+++ exam2.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
+using namespace std;
 int main() {
+    int cmb;
+    ofstream writefile ("std.txt", ofstream::app);
+    ifstream readfile ("std.txt");
+    string line;
+    string name;
 
+    cin >> cmb;
+
+    if (cmb == 1){
+        cin >> name;
+        if (writefile.is_open()){
+            writefile << name << endl;
+            writefile.close();
+        }else{
+                cout << "Unable to open file";
+        }
+    }else if (cmb == 2){
+        if (readfile.is_open()){
+            while (getline(readfile,line)){
+            cout << line << endl;
+            }
+            readfile.close();
+        }
+        }else{
+        }
         return 0;
 }
